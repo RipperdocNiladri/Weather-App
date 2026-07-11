d017f7ee703a836bd2b5401026e293f70329f5a6
Niladri Pal <niladripal0810@gmail.com>
2026-07-11 23:18:36 +0530

    Include user and location timestamp with extended weather data.

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@
 import requests
 import os
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # ==========================
 # Weather App
@@ -39,21 +39,38 @@ while True:
     try:
         response = requests.get(url)
         data = response.json()
+        current_time = datetime.now()
+
+        timezone_offset = data["timezone"]
+        utc_time = datetime.utcnow()
+        local_time = utc_time + timedelta(seconds=timezone_offset)
 
         if response.status_code == 200:
             city_name = data["name"]
             country = data["sys"]["country"]
 
+            latitude = data["coord"]["lat"]
+            longitude = data["coord"]["lon"]
+
             temperature = data["main"]["temp"]
             feels_like = data["main"]["feels_like"]
             humidity = data["main"]["humidity"]
             pressure = data["main"]["pressure"]
+            visibility = data["visibility"] / 1000
+            clouds = data["clouds"]["all"]
+            wind_degree = data["wind"]["deg"]
 
             weather = data["weather"][0]["main"]
             description = data["weather"][0]["description"]
             icon = weather_icons.get(weather, "🌍")
 
             wind_speed = data["wind"]["speed"]
+
+            formatted_date = current_time.strftime("%d %B %Y")
+            formatted_time = current_time.strftime("%I:%M:%S %p")
+
+            location_date = local_time.strftime("%d %B %Y")
+            location_time = local_time.strftime("%I:%M:%S %p")
 
             sunrise = data["sys"]["sunrise"]
             sunset = data["sys"]["sunset"]
@@ -63,19 +80,34 @@ while True:
 
 # Output
             print("╔════════════════════════════════════════════╗")
-            print("             🌤 WEATHER REPORT                ")
-            print("╠════════════════════════════════════════════╣")
-            print(f"📍City          : {city_name}, {country}")
-            print(f"🌍Weather       : {weather} {icon}")
-            print(f"📝Description   : {description}")
-            print(f"🌡️Temperature    : {temperature} °C")
-            print(f"🥵Feels Like    : {feels_like} °C")
-            print(f"💧Humidity      : {humidity}%")
-            print(f"🎈Pressure      : {pressure} hPa")
-            print(f"💨Wind Speed    : {wind_speed} m/s")
-            print(f"🌅 Sunrise      : {sunrise_time}")
-            print(f"🌇 Sunset       : {sunset_time}")
-            print("===============================================")
+            print("             ⛅ WEATHER REPORT                ")
+            print("╚════════════════════════════════════════════╝")
+            
+            print(f"📅 Your Date   : {formatted_date}")
+            print(f"🕒 Your Time   : {formatted_time}")
+
+            print("-" * 40)
+
+            print(f"📅 {city_name} Date : {location_date}")
+            print(f"🕒 {city_name} Time : {location_time}")
+
+            print("-" * 40)
+            
+            print(f"📍 City          : {city_name}, {country}")
+            print(f"🌐 Coordinates   : {latitude}, {longitude}")
+            print(f"🌍 Weather       : {weather} {icon}")
+            print(f"📝 Description   : {description}")
+            print(f"🌡️ Temperature    : {temperature} °C")
+            print(f"🥵 Feels Like    : {feels_like} °C")
+            print(f"💧 Humidity      : {humidity}%")
+            print(f"🎈 Pressure      : {pressure} hPa")
+            print(f"💨 Wind Speed    : {wind_speed} m/s")
+            print(f"🧭 Wint dir.     : {wind_degree}°")
+            print(f"☁️  Clouds        : {clouds}%")
+            print(f"👁️  Visibility    : {visibility} km")
+            print(f"🌅 Sunrise       : {sunrise_time}")
+            print(f"🌇 Sunset        : {sunset_time}")
+            print("═" * 40)
         else:
             print("City not found.")
     except Exception as e:
